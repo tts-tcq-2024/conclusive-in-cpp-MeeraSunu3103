92a309bc1f9a0591d9ecc14646fe3a701d1b4fe8
MeeraSunu3103 <Sunu.Meera@in.bosch.com>
2024-10-24 10:41:17 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -10,7 +10,8 @@ typedef enum {
 typedef enum {
   NORMAL,
   TOO_LOW,
-  TOO_HIGH
+  TOO_HIGH.
+  noOfBreachTypes
 } BreachType;
 
 BreachType inferBreach(double value, double lowerLimit, double upperLimit);
