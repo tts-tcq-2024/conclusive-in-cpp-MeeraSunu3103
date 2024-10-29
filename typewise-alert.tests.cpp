a4df54dd5b5f4eae1d865db068e1e86647722165
MeeraSunu3103 <Sunu.Meera@in.bosch.com>
2024-10-29 14:44:06 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -1,17 +1,5 @@
-#include <gtest/gtest.h>
-#include <gmock/gmock.h>
-#include "typewise-alert.h"
+#include "typewise-alert.tests.h"
 
-class mockClass {
-  public:
-    MOCK_METHOD(void, printStringToConsole, (std::string inputString), ());
-};
-
-mockClass* globalMockObject;
-
-void printStringToConsole(std::string inputString) {
-  globalMockObject->printStringToConsole(inputString);
-}
 
 TEST(TypeWiseAlertTestSuite,InfersBreachAccordingToLimits) {
 
